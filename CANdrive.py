d2546f714f6b9c31d54f3e755ef06aa912fe16a5
Srivatsav <109732969+srivatsavsurya@users.noreply.github.com>
2023-10-12 20:02:23 +0530

    Add files via upload

---
+++ CANdrive.py
@@ -0,0 +1,29 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import test_ui
+import sys
+
+class testGUI(QMainWindow, test_ui.Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.setButton.clicked.connect(self.set)
+        self.clearButton.clicked.connect(self.clear)
+
+    def set(self):
+        self.infoLabel.setText("Hello")
+
+    def clear(self):
+        self.infoLabel.setText("")
+
+        
+def main():
+    app = QApplication(sys.argv)
+    gui = testGUI()
+
+    gui.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
---
+++ ui_converter.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.check_output(['pyuic5','test.ui','-o','test_ui.py'])
